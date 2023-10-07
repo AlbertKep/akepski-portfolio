7099d27d9054e0791b0d19a29cc0848e6db92222
Albert <albertkepski3@gmail.com>
2023-10-07 11:12:38 +0200

    fix: add initial width to state

--- src/hooks/useCheckWidth.js
+++ src/hooks/useCheckWidth.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 const useCheckWidth = () => {
-  const [currentWidth, setCurrentWidth] = useState();
+  const [currentWidth, setCurrentWidth] = useState(window.innerWidth);
   useEffect(() => {
     const checkCurrentWidth = () => {
       setCurrentWidth(window.innerWidth);
